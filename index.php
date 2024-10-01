68545a343da39152b0e5176d26d8b4a5e7c4bb01
Genesis Alonso <genesisdevfh@gmail.com>
2024-09-30 19:38:38 -0700

    diseño del main index

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Certificados</title>
-</head>
-<body>
-    <h1>Sistema de Certificados</h1>
-    <ul>
-        <li><a href="guardar_datos.php">Registrar Certificado</a></li>
-        <li><a href="revisar_cursos.php">Revisar y Aprobar Certificado (Administrador)</a></li>
-        <li><a href="consulta_pdf.php">Consultar Certificados (Maestro)</a></li>
-    </ul>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Página Principal</title>
+    <link rel="stylesheet" href="../static/css/main.css" />
+  </head>
+  <body>
+    <div class="main-container">
+      <h1>Página Principal</h1>
+        <ul>
+            <li><a href="guardar_datos.php">Generar Constancia</a></li>
+            <li><a href="revisar_cursos.php">Administra Constancias (Administrador)</a></li>
+            <li><a href="consulta_pdf.php">Consultar Certificados (Maestro)</a></li>
+        </ul>
+    </div>
+    <div class="encabezado-itm">
+      <img
+        src="../static/img/encabezadoITMOFICIAL.jpeg"
+        alt="encabezado del itm"
+      />
+    </div>
+    <div class="diseño-top"></div>
+    <div class="diseño-bottom"></div>
 </body>
 </html>
